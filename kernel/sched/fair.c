075a21af271bce7e542ee65277c5734dfe55b9dc
Patrick Bellasi <patrick.bellasi@arm.com>
2017-11-08 15:50:57 +0000

    sched/fair: always used unsigned long for utilization
    Utilization and capacity are tracked as unsigned long, however some
    functions using them returns an int which is ultimately assigned back to
    unsigned long variables.
    
    Since there is not scope on using a different and signed type, this
    consolidate the signature of functions returning utilization to always
    use the native type.
    As well as improving code consistency this is expected also benefits
    code paths where utilizations should be clamped by voiding further type
    conversions or ugly type casts.
    
    Change-Id: Iebb7f98a859a3a61518a409967fec82957045ae2
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5359,7 +5359,7 @@ struct energy_env {
 	struct sched_group	*sg;
 };
 
-static int cpu_util_wake(int cpu, struct task_struct *p);
+static unsigned long cpu_util_wake(int cpu, struct task_struct *p);
 
 /*
  * __cpu_norm_util() returns the cpu util relative to a specific capacity,
@@ -6378,7 +6378,7 @@ done:
  * rq->curr. For that case we should return cpu util with contributions from
  * currently running task p removed.
  */
-static int cpu_util_wake(int cpu, struct task_struct *p)
+static unsigned long cpu_util_wake(int cpu, struct task_struct *p)
 {
 	unsigned long util, capacity;
 
